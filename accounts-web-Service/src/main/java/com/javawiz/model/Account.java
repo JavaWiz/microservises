1acf5278b230111b8a70874321eab7dca4462477
Ranjan Sai <mail2javawiz@gmail.com>
2019-07-21 12:51:55 +0530

    lombok usage

--- accounts-web-Service/src/main/java/com/javawiz/model/Account.java
+++ accounts-web-Service/src/main/java/com/javawiz/model/Account.java
@@ -2,64 +2,35 @@ package com.javawiz.model;
 
 import java.math.BigDecimal;
 
+import javax.annotation.Generated;
+
 import com.fasterxml.jackson.annotation.JsonRootName;
 import com.javawiz.service.WebAccountsService;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Account DTO - used to interact with the {@link WebAccountsService}.
  * 
- * @author Paul Chapman
  */
+@Getter
+@Setter
+@NoArgsConstructor
 @JsonRootName("Account")
 public class Account {
 
-	protected Long id;
-	protected String number;
-	protected String owner;
-	protected BigDecimal balance;
-
-	/**
-	 * Default constructor for JPA only.
-	 */
-	protected Account() {
-		balance = BigDecimal.ZERO;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	/**
-	 * Set JPA id - for testing and JPA only. Not intended for normal use.
-	 * 
-	 * @param id
-	 *            The new id.
-	 */
-	protected void setId(long id) {
-		this.id = id;
-	}
-
-	public String getNumber() {
-		return number;
-	}
-
-	protected void setNumber(String accountNumber) {
-		this.number = accountNumber;
-	}
-
-	public String getOwner() {
-		return owner;
-	}
-
-	protected void setOwner(String owner) {
-		this.owner = owner;
-	}
+	private long id;
+	private String number;
+	private String owner;
+	private BigDecimal balance = BigDecimal.ZERO;
 
 	public BigDecimal getBalance() {
 		return balance.setScale(2, BigDecimal.ROUND_HALF_EVEN);
 	}
 
-	protected void setBalance(BigDecimal value) {
+	public void setBalance(BigDecimal value) {
 		balance = value;
 		balance.setScale(2, BigDecimal.ROUND_HALF_EVEN);
 	}
@@ -68,5 +39,4 @@ public class Account {
 	public String toString() {
 		return number + " [" + owner + "]: $" + balance;
 	}
-
 }
