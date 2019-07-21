33cc8a87baaec2752dc56a79cdd70e60d6c4f3d9
Ranjan Sai <mail2javawiz@gmail.com>
2019-07-21 11:39:59 +0530

    update to latest spring boot version

--- eureka-server/src/main/java/com/javawiz/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/javawiz/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package com.javawiz.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
